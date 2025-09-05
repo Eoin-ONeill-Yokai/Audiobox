ccbd17bd123377f020fd8931f8da55ff41394d95
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-09-05 02:06:09 -0700

    Allow downgrades

--- scripts/buildtime/downgrade_wine.sh
+++ scripts/buildtime/downgrade_wine.sh
@@ -7,7 +7,7 @@ wine_version=9.21
 wine_variant=staging
 codename=$(shopt -s nullglob; awk '/^deb https:\/\/dl\.winehq\.org/ { print $3; exit 0 } END { exit 1 }' /etc/apt/sources.list /etc/apt/sources.list.d/*.list || awk '/^Suites:/ { print $2; exit }' /etc/apt/sources.list /etc/apt/sources.list.d/wine*.sources)
 suffix=$(dpkg --compare-versions "$wine_version" ge 6.1 && ((dpkg --compare-versions "$wine_version" eq 6.17 && echo "-2") || echo "-1"))
-apt install -y --install-recommends {"winehq-$wine_variant","wine-$wine_variant","wine-$wine_variant-amd64","wine-$wine_variant-i386"}="$wine_version~$codename$suffix"
+apt install -y --allow-downgrades --install-recommends {"winehq-$wine_variant","wine-$wine_variant","wine-$wine_variant-amd64","wine-$wine_variant-i386"}="$wine_version~$codename$suffix"
 
 # Prevent updates
 sudo apt-mark hold winehq-staging
